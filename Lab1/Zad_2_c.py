a471fe5b63c71c32deec168d3cdd0840e5d22547
w.buczak <wojtekbuczak1990@gmail.com>
2024-12-22 10:16:55 +0100

    zadanie 2c

---
+++ Lab1/Zad_2_c.py
@@ -0,0 +1,7 @@
+def wyswietl_liczby_parzyste(lista_liczb):
+    parzyste_liczby = [liczba for liczba in range(len(lista_liczb)) if liczba % 2 == 0]
+    print(parzyste_liczby)
+
+lista_liczb_10 = [32,12,65,23,69,44,1,7,31,100]
+
+wyswietl_liczby_parzyste(lista_liczb_10)
