e68b7b8addc84bfd6da1e2b607fc776e758b7ea4
w.buczak <wojtekbuczak1990@gmail.com>
2024-12-22 10:12:03 +0100

    zadanie 2b

---
+++ Lab1/Zad_2_b.py
@@ -0,0 +1,23 @@
+def lista_x_2v1(lista_liczb):
+    lista_liczb_x_2 = []
+    for i in range(len(lista_liczb)):
+        lista_liczb_x_2.insert(i, lista_liczb[i]*2)
+    return lista_liczb_x_2
+
+
+def lista_x_2v2(lista_liczb):
+    lista_liczb = [lista_liczb[liczba] * 2 for liczba in range(len(lista_liczb))]
+    return lista_liczb
+
+def wyswietl_liste(lista_liczb):
+    for element_listy in lista_liczb:
+        print(element_listy)
+
+lista_liczb_wej = [4, 20, 1, 69, 18]
+
+nowa_lista_a = lista_x_2v1(lista_liczb_wej)
+#wyswietl_liste(nowa_lista_a)
+print(nowa_lista_a)
+nowa_lista_b = lista_x_2v2(lista_liczb_wej)
+#wyswietl_liste(nowa_lista_b)
+print(nowa_lista_b)
