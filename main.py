638705139e42198d82332f4cbf33694de0cdcd64
w.buczak <wojtekbuczak1990@gmail.com>
2024-12-27 14:46:22 +0100

    dodanie drugiej biblioteki i funkcji cv2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PIL import Image
+import cv2
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 
-def odczytaj_tekst_z_obrazu(sciezka_do_obrazu):
-
+def odczytaj_tekst_z_obrazu_pil(sciezka_do_obrazu):
     try:
         obraz = Image.open(sciezka_do_obrazu)
         tekst = pytesseract.image_to_string(obraz, lang='pol')
@@ -13,8 +13,17 @@ def odczytaj_tekst_z_obrazu(sciezka_do_obrazu):
     except Exception as e:
         return f"Błąd podczas przetwarzania obrazu: {e}"
 
+def odczytaj_tekst_z_obrazu_cv2(sciezka_do_obrazu):
+    try:
+        obraz = cv2.imread(sciezka_do_obrazu)
+        tekst = pytesseract.image_to_string(obraz, lang='pol')
+        return tekst
+    except Exception as e:
+        return f"Błąd podczas przetwarzania obrazu: {e}"
 
-sciezka = ("obrazy\\retro.png")
-wynik = odczytaj_tekst_z_obrazu(sciezka)
+
+sciezka = ("obrazy\\nba1.jpg")
+wynik = odczytaj_tekst_z_obrazu_cv2(sciezka)
 print("Odczytany tekst:")
 print(wynik)
+
