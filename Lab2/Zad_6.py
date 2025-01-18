c54b47b485df3d97bc9486c02d854dbe409bba71
w.buczak <wojtekbuczak1990@gmail.com>
2025-01-18 21:59:04 +0100

    zad6

---
+++ Lab2/Zad_6.py
@@ -0,0 +1,15 @@
+def append_list(lista1: list, lista2: list) -> list:
+    lista = lista1 + lista2
+    lista2 = []
+    for element in lista:
+        if element not in lista2:
+            lista2.append(element)
+
+    for i in range(len(lista2) -1):
+        lista2[i] = lista2[i] ** 3
+    return lista2
+
+lista1 = [1, 2, 3, 4]
+lista2 = [4, 5, 6, 7]
+
+print(append_list(lista1, lista2))
