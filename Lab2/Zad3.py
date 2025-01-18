59c022a580673504e7615effc25223a8417ed4b1
w.buczak <wojtekbuczak1990@gmail.com>
2025-01-18 21:15:47 +0100

    zad3

---
+++ Lab2/Zad3.py
@@ -0,0 +1,13 @@
+def check_parity(liczba):
+    if (liczba%2) == 0:
+        parity = True
+    else:
+        parity = False
+    return parity
+
+liczba = int(input("Podaj licbe:"))
+if check_parity(liczba):
+    print("Liczba parzysta")
+else:
+    print("Liczba nieparzysta")
+
