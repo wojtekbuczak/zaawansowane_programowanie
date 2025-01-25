65bfc6f0bf145c3c1fd47ffc80619f055f84ef81
w.buczak <wojtekbuczak1990@gmail.com>
2025-01-25 21:32:07 +0100

    poprawa kolejkowania na asynchroniczne

--- copy_img.py
+++ copy_img.py
@@ -6,7 +6,7 @@ def copy_image(image_path, output_folder, copies, prefix="test"):
 
     os.makedirs(output_folder, exist_ok=True)
 
-    for i in range(2, copies + 2):  # Zaczynamy od "test2" do "test201"
+    for i in range(2, copies + 2):
         # Tworzymy nową nazwę pliku
         new_name = f"{prefix}{i}.jpg"
         new_path = os.path.join(output_folder, new_name)
@@ -19,6 +19,6 @@ def copy_image(image_path, output_folder, copies, prefix="test"):
 # Przykład użycia
 original_image = r"C:\\do_przerzucenia\\studia\\programowanie\\person_pics\\test.jpg"  # Zamień na rzeczywistą ścieżkę
 output_directory = "C:\\do_przerzucenia\\studia\\programowanie\\person_pics"  # Zamień na rzeczywistą ścieżkę
-number_of_copies = 100
+number_of_copies = 500
 
 copy_image(original_image, output_directory, number_of_copies)
